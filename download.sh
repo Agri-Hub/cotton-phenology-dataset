9e34586dac67eb55ab1abccccaeeff484e5f746e
Alkis Koukos <38884027+alkiskk@users.noreply.github.com>
2023-03-09 17:34:26 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,15 +2,15 @@
 function downloadPhenology {
   mkdir -p data
   cd data
-  wget https://zenodo.org/record/7646864/files/in_situ_cotton_phenology_labels.csv?download=1
-  wget https://zenodo.org/record/7646864/files/parcels.geojson?download=1
+  wget https://zenodo.org/record/7646864/files/in_situ_cotton_phenology_labels.csv
+  wget https://zenodo.org/record/7646864/files/parcels.geojson
   cd ..
 }
 
 function downloadFieldImages {
   mkdir -p data/images
   cd data/images
-  wget https://zenodo.org/record/7646864/files/cotton_phenology_in-situ_photos_2021.zip?download=1
+  wget https://zenodo.org/record/7646864/files/cotton_phenology_in-situ_photos_2021.zip
   unzip cotton_phenology_in-situ_photos_2021.zip
   rm cotton_phenology_in-situ_photos_2021.zip
   cd ..
